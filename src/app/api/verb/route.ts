75b68f06d06bdc52056e4c927c736fa8cb01faa6
Bo Gustavsson <bosse@gustavsson.one>
2025-04-27 00:50:40 +0200

    Add node-cron for scheduled verb updates and implement verb retrieval logic

---
+++ src/app/api/verb/route.ts
@@ -0,0 +1,28 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import { NextResponse, NextRequest } from 'next/server';
+import { getRandomVerb } from '../../../lib/getNewVerb';
+import cron from 'node-cron';
+
+var verb: string;
+verb = getRandomVerb();
+
+
+cron.schedule('0 0 * * *', async () => {
+  try {
+    verb = getRandomVerb();
+    console.log('Random verb fetched:', verb);
+
+} catch (error) {
+    console.error('Error fetching random verb at midnight:', error);
+  }
+});
+
+export async function GET(request: NextRequest) {
+  try {
+    return NextResponse.json(verb);
+  } catch (error) {
+    console.error('Error reading verbs.json:', error);
+    return NextResponse.json({ error: 'Failed to load verbs' }, { status: 500 });
+  }
+}
---
+++ src/lib/getNewVerb.ts
@@ -0,0 +1,39 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+   
+   
+const filePath = path.resolve(process.cwd(), 'src/lib/verbs.json');
+const file = await fs.readFile(filePath, 'utf8');
+const data = JSON.parse(file);
+const verbs = data.verb;
+const usedVerbs = data.used
+
+
+const getRandomVerb = () => {
+    const randomIndex = Math.floor(Math.random() * verbs.length);
+
+    const randomVerbTry = verbs[randomIndex];
+
+    for (let i = 0; i < usedVerbs.length; i++) {
+        if (usedVerbs[i] === randomVerbTry) {
+            return getRandomVerb();
+        }
+    }
+    usedVerbs.push(randomVerbTry);
+    const updatedData = {
+        verb: verbs,
+        used: usedVerbs
+    };
+    fs.writeFile(filePath, JSON.stringify(updatedData), 'utf8')
+        .then(() => {
+            console.log('File updated successfully');
+        })
+        .catch((error) => {
+            console.error('Error writing file:', error);
+        });
+    const randomVerb = randomVerbTry;
+
+    return randomVerb;
+}
+
+export { getRandomVerb };
